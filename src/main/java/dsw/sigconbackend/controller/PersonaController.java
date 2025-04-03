7527a7421dbaa43031b50bec1bc20b6a834d67f3
F20LAB207E18 <F20LAB207E18@192.168.127.56>
2025-04-02 20:46:53 -0500

    persona controller

--- src/main/java/dsw/sigconbackend/controller/PersonaController.java
+++ src/main/java/dsw/sigconbackend/controller/PersonaController.java
@@ -1,10 +1,16 @@
 package dsw.sigconbackend.controller;
 
+import dsw.sigconbackend.service.PersonaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path="api/v1/persona")
 public class PersonaController {
-    
+    private final Logger logger=LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    PersonaService personaService;
 }
